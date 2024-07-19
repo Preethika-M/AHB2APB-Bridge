89121c5a8628ed6d9c17180b0519d545d0f55361
Preethika <preethikamurugan41@gmail.com>
2024-07-19 18:32:40 +0530

    Update AHB_master.v

--- AHB_master.v
+++ AHB_master.v
@@ -82,4 +82,75 @@ begin   #21;
 end
 endtask
 
+task burst_read();
+begin   
+    #21;
+    Hresetn = 1;
+    Htrans = 2'b10;  // Non-sequential transfer
+    Hreadyin = 1;
+    Haddr = 32'h80004001;
+    Hwrite = 0;
+
+    #20;
+    Htrans = 2'b11;  // Sequential transfer
+    Hreadyin = 1;
+    Haddr = 32'h80004002;
+    Hwrite = 0;
+
+    #20;
+    Htrans = 2'b11;  // Sequential transfer
+    Hreadyin = 1;
+    Haddr = 32'h80004003;
+    Hwrite = 0;
+
+    #20;
+    Htrans = 2'b11;  // Sequential transfer
+    Hreadyin = 1;
+    Haddr = 32'h80004004;
+    Hwrite = 0;
+
+    #20;
+    Htrans = 2'b00;  // Idle transfer
+    Hreadyin = 0;
+end
+endtask
+
+task wrap_write();
+    integer j;
+    begin
+        @(posedge Hclk)
+        #1;
+        begin
+            Hwrite = 1;
+            Htrans = 2'b10;
+            Hsize = 3'b001;  // Assuming size 8-bit
+            Hburst = 3'b011; // Assuming WRAP burst
+            Hreadyin = 1;
+            Haddr = 32'h8000_0010;
+            Hwdata = $random % 256; // Initial data
+        end
+
+        for (j = 0; j < 4; j = j + 1) begin // Adjusted burst length for wrap
+            @(posedge Hclk)
+            #1;
+            begin
+                Htrans = 2'b11; // Incremental burst
+                Haddr = {Haddr[31:3], Haddr[2:0] + 3'b001}; // Adjust for wrap
+                Hwdata = $random % 256; // Random data to write
+
+                // Introduce wait state
+                @(posedge Hclk);
+                #1;
+                Hreadyin = 0;
+                @(posedge Hclk);
+                #1;
+                Hreadyin = 1;
+            end
+        end
+        Htrans = 2'b00; // End burst
+    end
+endtask
+
+
+
 endmodule
